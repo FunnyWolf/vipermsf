837044f3c744485460a098c2239be34f521b6678
FunnyWolf <yu5890681@gmail.com>
2021-02-28 21:01:35 +0800

    修复socks模块加载失败问题

--- modules/auxiliary/server/socks5_api.rb
+++ modules/auxiliary/server/socks5_api.rb
@@ -3,43 +3,40 @@
 # Current source: https://github.com/rapid7/metasploit-framework
 ##
 
-require 'thread'
-require 'rex/proto/proxy/socks5'
-
 class MetasploitModule < Msf::Auxiliary
   include Msf::Auxiliary::Report
 
   def initialize
     super(
-      'Name'           => 'Socks5 Proxy Server',
-      'Description'    => %q{
+            'Name'           => 'Socks5 Proxy Server',
+            'Description'    => %q{
         This module provides a socks5 proxy server that uses the builtin
         Metasploit routing to relay connections.
       },
-      'Author'         => [ 'sf', 'Spencer McIntyre', 'surefire' ],
-      'License'        => MSF_LICENSE,
-      'Actions'        =>
-        [
-          [ 'Proxy' ]
-        ],
-      'PassiveActions' =>
-        [
-          'Proxy'
-        ],
-      'DefaultAction'  => 'Proxy'
+            'Author'         => ['sf', 'Spencer McIntyre', 'surefire'],
+            'License'        => MSF_LICENSE,
+            'Actions'        =>
+                    [
+                            ['Proxy']
+                    ],
+            'PassiveActions' =>
+                    [
+                            'Proxy'
+                    ],
+            'DefaultAction'  => 'Proxy'
     )
 
     register_options([
-      OptString.new('USERNAME', [false, 'Proxy username for SOCKS5 listener']),
-      OptString.new('PASSWORD', [false, 'Proxy password for SOCKS5 listener']),
-      OptString.new('SRVHOST',  [true,  'The address to listen on', '0.0.0.0']),
-      OptPort.new('SRVPORT',    [true,  'The port to listen on', 1080])
-    ])
+                             OptString.new('USERNAME', [false, 'Proxy username for SOCKS5 listener']),
+                             OptString.new('PASSWORD', [false, 'Proxy password for SOCKS5 listener']),
+                             OptString.new('SRVHOST', [true, 'The address to listen on', '0.0.0.0']),
+                             OptPort.new('SRVPORT', [true, 'The port to listen on', 1080])
+                     ])
   end
 
   def setup
     super
-    @mutex = ::Mutex.new
+    @mutex       = ::Mutex.new
     @socks_proxy = nil
   end
 
@@ -55,12 +52,12 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   def run
-    opts = {
-      'ServerHost'     => datastore['SRVHOST'],
-      'ServerPort'     => datastore['SRVPORT'],
-      'ServerUsername' => datastore['USERNAME'],
-      'ServerPassword' => datastore['PASSWORD'],
-      'Context'        => {'Msf' => framework, 'MsfExploit' => self}
+    opts         = {
+            'ServerHost'     => datastore['SRVHOST'],
+            'ServerPort'     => datastore['SRVPORT'],
+            'ServerUsername' => datastore['USERNAME'],
+            'ServerPassword' => datastore['PASSWORD'],
+            'Context'        => { 'Msf' => framework, 'MsfExploit' => self }
     }
     @socks_proxy = Rex::Proto::Proxy::Socks5::Server.new(opts)
 
