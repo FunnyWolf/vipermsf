8a02e96759ef91ebfa65cc89ad5e17c3d645b4f2
FunnyWolf <yu5890681@gmail.com>
2021-09-04 13:21:22 +0800

    屏蔽session检查

--- lib/msf/core/post_mixin.rb
+++ lib/msf/core/post_mixin.rb
@@ -38,10 +38,10 @@ module Msf::PostMixin
     # for its platform, capabilities, etc.
     check_for_session_readiness if session.type == "meterpreter"
 
-    incompatibility = session_incompatibility(session)
-    unless incompatibility.nil?
-      print_warning("SESSION may not be compatible with this module (#{incompatibility})")
-    end
+    # incompatibility = session_incompatibility(session)
+    # unless incompatibility.nil?
+    #   print_warning("SESSION may not be compatible with this module (#{incompatibility})")
+    # end
 
     # Msf::Exploit#setup for exploits, NoMethodError for post modules
     super rescue NoMethodError
@@ -221,9 +221,6 @@ module Msf::PostMixin
                   Rex::Post::Meterpreter::ClientCore.extension_id + Rex::Post::Meterpreter::COMMAND_ID_RANGE - 1
           )
         end
-      end
-      if s.base_platform == 'linux'
-        return nil
       end
       missing_cmd_ids = (cmd_ids - s.commands)
       unless missing_cmd_ids.empty?
