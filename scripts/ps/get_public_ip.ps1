35c0aaab589acc74d91e9e5dc6d77eee01e01aea
rootkit <yu5890681@gmail.com>
2021-09-19 22:23:53 +0800

    add module `Obtain Internet outbound IP` 新增模块`获取互联网出口IP`

---
+++ scripts/ps/get_public_ip.ps1
@@ -0,0 +1,8 @@
+$WebRequest = [System.Net.WebRequest]::Create("https://ifconfig.me/ip")
+$WebRequest.Method = "GET"
+$WebRequest.ContentType = "application/json"
+$Response = $WebRequest.GetResponse()
+$ResponseStream = $Response.GetResponseStream()
+$ReadStream = New-Object System.IO.StreamReader $ResponseStream
+$Data = $ReadStream.ReadToEnd()
+Write-Host $Data
