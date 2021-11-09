19e12b4df84f48efdde79fb6f969c265df6d2a01
FunnyWolf <yu5890681@gmail.com>
2021-11-09 17:04:41 +0800

    create puma conf

---
+++ config/puma.rb
@@ -0,0 +1,4 @@
+environment 'production'
+quiet
+threads 0, 64
+preload_app!
---
+++ config/puma_production.rb
@@ -0,0 +1,12 @@
+application_path = "/root/metasploit-framework"
+LOGDIR = "/root/viper/Docker/log"
+msgrpc_port = 60005
+directory application_path
+environment 'production'
+pidfile "#{application_path}/puma.pid"
+stdout_redirect "#{LOGDIR}/puma.log","#{LOGDIR}/puma.log"
+rackup '/root/metasploit-framework/msf-json-rpc.ru'
+quiet
+threads 0, 64
+bind "tcp://127.0.0.1:#{msgrpc_port}"
+preload_app!
