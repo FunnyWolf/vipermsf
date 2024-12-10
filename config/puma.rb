fe7965250576232b7ec3164b392522817bf7b561
rootkit <yu5890681@gmail.com>
2024-12-10 10:47:44 +0800

    debug use diff port

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,12 @@
-environment 'production'
+application_path = "/root/metasploit-framework"
+LOGDIR = "/root/viper/Docker/log"
+msgrpc_port = 55553
+directory application_path
+# environment 'production'
+pidfile "#{application_path}/puma.pid"
+stdout_redirect "#{LOGDIR}/puma.log", "#{LOGDIR}/puma.log"
+rackup '/root/metasploit-framework/msf-json-rpc.ru'
 quiet
-threads 0, 64
-preload_app!
+threads 0, 16
+bind "tcp://127.0.0.1:#{msgrpc_port}"
+# preload_app!
